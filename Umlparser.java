bc65a82c36ccf4a8203d08a04af9d458a9854489
Sneha <snehaa.94@gmail.com>
2017-02-17 23:11:47 -0800

    Add files via upload

---
+++ Umlparser.java
@@ -0,0 +1,49 @@
+import java.io.File;
+public class Umlparser {
+	//private String fileName = "";
+	public static void main (String[] args) throws Exception {
+	
+		String sourceFolder = "";
+		String fileName = "";
+		String  parserOutput = "";
+		if (args.length == 2)
+		{
+			sourceFolder = args[0];
+			fileName = args[1];	
+			Umlparser u = new Umlparser();
+			u.getJavaSourceFiles(sourceFolder);
+			//parserOutput = u.parseJava(sourceFolder);
+			//u.generateUML(parserOutput);
+			  
+		}
+		else
+		{
+			System.out.println("Please provide input in correct format - <source folder><output file name>" );
+		}	
+		
+	}
+	public String getJavaSourceFiles(String sourceFolder)
+	{
+		File srcFolder = new File(sourceFolder);
+		if(srcFolder.isDirectory())
+		{
+			File[] allFiles = srcFolder.listFiles();			
+			if (allFiles != null) {
+				for (File f : allFiles) {
+					if(f.getName().endsWith(".java")){
+					parseJava(f);
+					}
+			    }	
+			}
+		}
+	}
+	
+	public void parseJava(File javaSorceFile){
+		//call parser
+	}
+	public void generateUML(String parserOutput)
+	{
+		//call uml generator
+	}
+	}
+}
