e861bb41eec2bd0f5c57ea49bba536a0ff6bf552
Sneha <snehaa.94@gmail.com>
2017-03-24 23:37:26 -0700

    Update Umlparser.java

--- UMLParser/src/Umlparser.java
+++ UMLParser/src/Umlparser.java
@@ -13,9 +13,8 @@ public class Umlparser {
 		String sourceFolder = "";
 		String fileName = "";
 		// Get inputs from command line args
-		if (args.length == 2) {
-			sourceFolder = "C:\\Users\\sneha_2\\Desktop\\CMPE202\\chk";
-			// sourceFolder = args[0];
+		if (args.length == 2) {			
+			sourceFolder = args[0];
 			fileName = args[1];
 			ParseJava p = new ParseJava();
 			p.getJavaSourceFiles(sourceFolder);
