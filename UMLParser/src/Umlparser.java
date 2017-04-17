be87a1221b5df2b7acf979aaf98ed09fdeab6920
Sneha <snehaa.94@gmail.com>
2017-04-17 12:56:59 -0700

    Update Umlparser.java

--- UMLParser/src/Umlparser.java
+++ UMLParser/src/Umlparser.java
@@ -6,23 +6,24 @@
  * @author sneha_2
  *
  */
-import java.io.File;
+
 public class Umlparser {
+
 	public static void main(String[] args) throws Exception {
-		String sourceFolder = "";
-		String fileName = "";
-		// Get inputs from command line args
-		if (args.length == 2) {			
-			sourceFolder = args[0];
-			fileName = args[1];
-			ParseClass p = new ParseClass(sourceFolder, fileName);
-		        p.getJavaSourceFiles();
-			
-		} 
-		else {
-			// incorrect input format
-			System.out.println("Please provide input in correct format - <source folder><output file name>");
+		// Get inputs
+		try {
+			if (args.length == 2) {
+				String sourceFolder = args[0];
+				String fileName = args[1];
+				ParseClass p = new ParseClass(sourceFolder, fileName);
+				p.getJavaSourceFiles();
+
+			} else {
+				System.out.println("Please provide input in correct format - <sourcefolder> <output file name>");
+			}
+		} catch (Exception e) {
+			System.out.println("Incorrect Input format");
 		}
 	}
-	
+
 }
